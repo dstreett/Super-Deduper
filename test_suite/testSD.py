b17df2db7fffbf07e11630755fc249efaa5ee0a3
Christopher Mirabzadeh <mira2978@vandals.uidaho.edu>
2016-05-02 21:53:01 -0700

    working unit test

--- test_suite/testSD.py
+++ test_suite/testSD.py
@@ -10,38 +10,33 @@ def findFastqFiles(directory, pattern):
             if fnmatch.fnmatch(basename, pattern):
                 filename = os.path.join(root, basename)
                 filenames.append(filename)
+    filenames.sort()
     return filenames
 
 def interLeaved():
     # testing expected output
-    command = sys_cmd + " -1 fastqFiles/testCase_1X_R1.fastq -2 fastqFiles/testCase_1X_R2.fastq  -o interleaved_out"
+    command = "../super_deduper -1 fastqFiles/testCase_1X_R1.fastq -2 fastqFiles/testCase_1X_R2.fastq  -o interleaved_out"
+    subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
     return filecmp.cmp('interleaved_out_nodup_PE1.fastq', 'expected_interleaved_nodup_R1.fastq') 
     
 
 class SuperDeduperTestCase(unittest.TestCase):
+
     def test_find_fastq_files_recursively(self):
         """Should return all fastq files from the sub directories"""
-        self.assertEqual(findFastqFiles('fastqFiles', '*.fastq'), 
+        self.assertEqual(findFastqFiles('fastqFiles', '*.fastq'),
                          ['fastqFiles/testCase_1X_R1.fastq', 
                           'fastqFiles/testCase_1X_R2.fastq', 
                           'fastqFiles/testCase_2X_R1.fastq',
-                          'fastqFiles/testCase_2X_R1.fastq',
+                          'fastqFiles/testCase_2X_R2.fastq',
                           'fastqFiles/testCase_3X_R1.fastq',
-                          'fastqFiles/testCase_3X_R1.fastq'])
+                          'fastqFiles/testCase_3X_R2.fastq'])
     
-    def test_for_out_of_bounds(self):
-        self.assertEqual(interLeaved(),[True])
     
-    #def test_1X_duplicates(self):
         
         
-    #def test_2X_duplicates(self):
-        
-        
-    #def test_3X_duplicates(self):
         
 
-    #def test_interleaved(self):
         
         
 if __name__ == '__main__':
