afc33650e35a6d624b21ed0a5d6ec505f81f8f2e
biosandman <mira2978@vandals.uidaho.edu>
2016-05-09 13:36:08 -0700

    made changes to add standard tests

--- test_suite/testSD.py
+++ test_suite/testSD.py
@@ -1,9 +1,12 @@
 import unittest
-import os, fnmatch, sys, subprocess
+import os
+import fnmatch
+import sys
+import subprocess
 import filecmp
 
 # find all files, output names
-def findFastqFiles(directory, pattern):
+def find_fastq_files(directory, pattern):
     """Walks the directory structure, appending filenames to an array"""
     filenames = []
     for root, dirs, files in os.walk(directory):
@@ -14,38 +17,54 @@ def findFastqFiles(directory, pattern):
     filenames.sort()
     return filenames
 
-def interLeaved():
-    # testing expected output
-    command = "../super_deduper -1 fastqFiles/testCase_1X_R1.fastq -2 fastqFiles/testCase_1X_R2.fastq  -i -F -N"
-    subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-    return filecmp.cmp('no_dup_interleaved.fastq', 'expected_interleaved.fastq')
+# basic call to the shell application we are testing
+def sub_process(command):
+    return subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
 
-def duplicateReads():
-    command = "../super_deduper -1 fastqFiles/testCase_1X_R1.fastq -2 fastqFiles/testCase_1X_R1.fastq -N -F"
-    myString =  subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-    return myString
+def file_compare(command, expected, returned):
+    # testing expected file output
+    subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    return filecmp.cmp(expected, returned)
 
 
 class TestCase(unittest.TestCase):
 
     def test_find_fastq_files_recursively(self):
         """Should return all fastq files from the sub directories"""
-        self.assertEqual(findFastqFiles('fastqFiles', '*.fastq'),
+        self.assertEqual(find_fastq_files('fastqFiles', '*.fastq'),
                          ['fastqFiles/testCase_1X_R1.fastq',
-                          'fastqFiles/testCase_1X_R2.fastq',
-                          'fastqFiles/testCase_2X_R1.fastq',
-                          'fastqFiles/testCase_2X_R2.fastq',
-                          'fastqFiles/testCase_3X_R1.fastq',
-                          'fastqFiles/testCase_3X_R2.fastq'])
+                          'fastqFiles/testCase_1X_R2.fastq'])
 
-    def test_duplicate_reads(self):
-        """Should return a tab delimited out put with 11 reads and 10 writes"""
-        self.assertEqual(duplicateReads(),
-        'Reads_Read\tReads_Written\tReads_Discarded\tSingletons\tDoubles\tThree_Plus\tDisqualifed_Reads\tReplacements_Called\tTotal_Time\n12\t11\t1\t10\t1\t0\t0\t0\t0\n')
+    # copy this def to make new command tests
+    def test_basic_input(self):
+        """Should return that basic input works"""
+        myR1file = " -1 fastqFiles/testCase_1X_R1.fastq "
+        myR2file = "-2 fastqFiles/testCase_1X_R2.fastq "
+        additFlags = "-N -F"
+        myShellCmd = "../super_deduper"
+        myCommand = myShellCmd+myR1file+myR2file+additFlags
+        self.assertIn("Reads_Read",sub_process(myCommand))
 
-    def test_out_interleaved(self):
-		"""Should compare the output file to the expected_interleaved"""
-		self.assertTrue(interLeaved())
+    def test_specific_output(self):
+        """Should return a tab delimited out put"""
+        myR1file = " -1 fastqFiles/testCase_1X_R1.fastq "
+        myR2file = "-2 fastqFiles/testCase_1X_R2.fastq "
+        additFlags = "-N -F"
+        myShellCmd = "../super_deduper"
+        myCommand = myShellCmd+myR1file+myR2file+additFlags
+        myExpectedOutput = 'Reads_Read\tReads_Written\tReads_Discarded\tSingletons\tDoubles\tThree_Plus\tDisqualifed_Reads\tReplacements_Called\tTotal_Time\n12\t11\t1\t10\t1\t0\t0\t0\t0\n'
+
+    def test_file_compare(self):
+        """Should  return that two files match line for line"""
+        myR1file = " -1 fastqFiles/testCase_1X_R1.fastq "
+        myR2file = "-2 fastqFiles/testCase_1X_R2.fastq "
+        additFlags = "-F -N"
+        myShellCmd = "../super_deduper"
+        myCommand = myShellCmd+myR1file+myR2file+additFlags
+        myExpectedFile = "expected_R1.fastq"
+        myReturnedFile = "no_dup_R1.fastq"
+        self.assertTrue(file_compare(myCommand, myExpectedFile, myReturnedFile))
+
 
 
 
