d3560635e39c3ccdf15ffa73ef8966d89ca0e0e8
biosandman <mira2978@vandals.uidaho.edu>
2016-05-16 12:46:36 -0700

    preparing for merge request

--- test_suite/testSD.py
+++ test_suite/testSD.py
@@ -1,11 +1,12 @@
 import unittest
 import os
 import fnmatch
-import sys
 import subprocess
 import filecmp
 
 # find all files, output names
+
+
 def find_fastq_files(directory, pattern):
     """Walks the directory structure, appending filenames to an array"""
     filenames = []
@@ -18,21 +19,27 @@ def find_fastq_files(directory, pattern):
     return filenames
 
 # basic call to the shell application we are testing
+
+
 def sub_process(command):
-    return subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    return subprocess.check_output(
+        command, stderr=subprocess.STDOUT, shell=True)
+
 
 def file_compare(command, expected, returned):
     # testing expected file output
     subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
     return filecmp.cmp(expected, returned)
 
+
 def parse_fastq(filename):
+    # output the file to a dictionary
     with open(filename) as f:
-        lines=f.readlines()
-    head=[item[:-1] for item in lines[::4]] #get rid of '\n'
-    read=[item[:-1] for item in lines[1::4]]
-    qual=[item[:-1] for item in lines[3::4]]
+        lines = f.readlines()
+    read = [item[:-1] for item in lines[1::4]]
+    qual = [item[:-1] for item in lines[3::4]]
     return dict(zip(read, qual))
+
 
 class TestCase(unittest.TestCase):
 
@@ -49,8 +56,8 @@ class TestCase(unittest.TestCase):
         myR2file = "-2 fastqFiles/testCase_1X_R2.fastq "
         additFlags = "-N -F"
         myShellCmd = "../super_deduper"
-        myCommand = myShellCmd+myR1file+myR2file+additFlags
-        self.assertIn("Reads_Read",sub_process(myCommand))
+        myCommand = myShellCmd + myR1file + myR2file + additFlags
+        self.assertIn("Reads_Read", sub_process(myCommand))
 
     def test_specific_output(self):
         """Should return a tab delimited out put"""
@@ -58,8 +65,13 @@ class TestCase(unittest.TestCase):
         myR2file = "-2 fastqFiles/testCase_1X_R2.fastq "
         additFlags = "-N -F"
         myShellCmd = "../super_deduper"
-        myCommand = myShellCmd+myR1file+myR2file+additFlags
-        myExpectedOutput = 'Reads_Read\tReads_Written\tReads_Discarded\tSingletons\tDoubles\tThree_Plus\tDisqualifed_Reads\tReplacements_Called\tTotal_Time\n12\t11\t1\t10\t1\t0\t0\t0\t0\n'
+        myCommand = myShellCmd + myR1file + myR2file + additFlags
+        myExpectedOutput = ("Reads_Read\tReads_Written\tReads_Discarded\t"
+                            "Singletons\tDoubles\tThree_Plus\t"
+                            "Disqualifed_Reads\tReplacements_Called\t"
+                            "Total_Time\n12\t11\t1\t10\t"
+                            "1\t0\t0\t0\t0\n")
+        self.assertEqual(sub_process(myCommand), myExpectedOutput)
 
     def test_file_compare(self):
         """Should  return that two files match line for line"""
@@ -67,15 +79,17 @@ class TestCase(unittest.TestCase):
         myR2file = "-2 fastqFiles/testCase_1X_R2.fastq "
         additFlags = "-F -N"
         myShellCmd = "../super_deduper"
-        myCommand = myShellCmd+myR1file+myR2file+additFlags
+        myCommand = myShellCmd + myR1file + myR2file + additFlags
         myExpectedFile = "expected_R1.fastq"
         myReturnedFile = "no_dup_R1.fastq"
-        self.assertTrue(file_compare(myCommand, myExpectedFile, myReturnedFile))
+        self.assertTrue(file_compare(
+            myCommand, myExpectedFile, myReturnedFile))
 
     def test_item_from_one_exists_in_two(self):
-        """Should return that an entry in the expected output exists in the input"""
-        data01=parse_fastq("expected_R1.fastq")
-        data02=parse_fastq("fastqFiles/testCase_1X_R1.fastq")
+        """Should return that an entry in the expected output """
+        """exists in the input"""
+        data01 = parse_fastq("expected_R1.fastq")
+        data02 = parse_fastq("fastqFiles/testCase_1X_R1.fastq")
         self.assertTrue(data01.items()[0][0] in data02)
 
 
