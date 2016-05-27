fb34af27356c3d75fe912a635c38fe6e30599403
U-CM_LaptopPC\ChrisM <camirabzadeh@gmail.com>
2016-05-26 19:42:28 -0700

    added check for reverse complement

--- test_suite/testSD.py
+++ test_suite/testSD.py
@@ -3,6 +3,7 @@ import os
 import fnmatch
 import subprocess
 import filecmp
+import time
 
 # find all files, output names
 
@@ -36,75 +37,49 @@ def parse_fastq(filename):
     # output the file to a dictionary
     with open(filename) as f:
         lines = f.readlines()
+    head = [item[:-1] for item in lines[0::4]]
     read = [item[:-1] for item in lines[1::4]]
     qual = [item[:-1] for item in lines[3::4]]
-    return dict(zip(read, qual))
+    return {'Headers': head, 'Sequences': read, 'QScores': qual}
 
 
 class TestCase(unittest.TestCase):
 
+    def setUp(self):
+        """Setup any thing used in muliple tests"""
+        myR1file = " -1 fastqFiles/testCase_1X_R1.fastq "
+        myR2file = " -2 fastqFiles/testCase_1X_R2.fastq "
+        additFlags = "-N -F"
+        myShellCmd = "../super_deduper"
+        self.myCommand = myShellCmd + myR1file + myR2file + additFlags
+
     def test_find_fastq_files_recursively(self):
         """Should return all fastq files from the sub directories"""
         self.assertEqual(find_fastq_files('fastqFiles', '*.fastq'),
-                         ['fastqFiles/testCase_1X_R1.fastq',
+                         ['fastqFiles/expected_R1.fastq',
+                          'fastqFiles/testCase_1X_R1.fastq',
                           'fastqFiles/testCase_1X_R2.fastq'],
                          "Unable to find test files")
 
-    # copy this def to make new command tests
-    def test_basic_input(self):
+    def test_tab_output(self):
         """Tests for 'Reads_Read' in the output"""
-        myR1file = " -1 fastqFiles/testCase_1X_R1.fastq "
-        myR2file = "-2 fastqFiles/testCase_1X_R2.fastq "
-        additFlags = "-N -F"
-        myShellCmd = "../super_deduper"
-        myCommand = myShellCmd + myR1file + myR2file + additFlags
-        self.assertIn("Reads_Read", sub_process(myCommand),
-                      "Expected to see 'Reads_Read' in output "
+        self.assertIn("Reads_Read\tReads_Written\tReads_Discarded\t"
+                      "Singletons\tDoubles\tThree_Plus\t"
+                      "Disqualifed_Reads\tReplacements_Called\t"
+                      "Total_Time",
+                      sub_process(self.myCommand),
+                      "Unexpected tab structure "
                       "super_deduper -1 fastqFiles/testCase_1X_R1.fastq"
                       "-2 fastqFiles/testCase_1X_R2.fastq"
                       "-F -N")
 
-    def test_specific_output(self):
-        """Tests for tab delimited output"""
-        myR1file = " -1 fastqFiles/testCase_1X_R1.fastq "
-        myR2file = "-2 fastqFiles/testCase_1X_R2.fastq "
-        additFlags = "-N -F"
-        myShellCmd = "../super_deduper"
-        myCommand = myShellCmd + myR1file + myR2file + additFlags
-        myExpectedOutput = ("Reads_Read\tReads_Written\tReads_Discarded\t"
-                            "Singletons\tDoubles\tThree_Plus\t"
-                            "Disqualifed_Reads\tReplacements_Called\t"
-                            "Total_Time\n12\t11\t1\t10\t"
-                            "1\t0\t0\t0\t0\n")
-        self.assertEqual(sub_process(myCommand), myExpectedOutput,
-                         "Unexpected tab delimited output "
-                         "super_deduper -1 fastqFiles/testCase_1X_R1.fastq"
-                         "-2 fastqFiles/testCase_1X_R2.fastq"
-                         "-F -N")
-
-    def test_file_compare(self):
-        """Tests to compare output file matches expected out"""
-        myR1file = " -1 fastqFiles/testCase_1X_R1.fastq "
-        myR2file = "-2 fastqFiles/testCase_1X_R2.fastq "
-        additFlags = "-F -N"
-        myShellCmd = "../super_deduper"
-        myCommand = myShellCmd + myR1file + myR2file + additFlags
-        myExpectedFile = "expected_R1.fastq"
-        myReturnedFile = "no_dup_R1.fastq"
-        self.assertTrue(file_compare(
-            myCommand, myExpectedFile, myReturnedFile),
-            "Files do no match "
-            "super_deduper -1 fastqFiles/testCase_1X_R1.fastq"
-            "-2 fastqFiles/testCase_1X_R2.fastq"
-            "-F -N")
-
     def test_item_from_one_exists_in_two(self):
         """Tests if the first entry in the expected output is in the input"""
-        data01 = parse_fastq("expected_R1.fastq")
-        data02 = parse_fastq("fastqFiles/testCase_1X_R1.fastq")
-        self.assertTrue(data01.items()[0][0] in data02,
-                        "The first entry was in expected output "
-                        "is not found in the testcase")
+        sub_process(self.myCommand)
+        data01 = parse_fastq("no_dup_R1.fastq")
+        self.assertIn("@higher_qscore_duplicate_of_read_one",
+                      data01['Headers'],
+                      "The output does not contain the expected sequence")
 
 
 if __name__ == '__main__':
